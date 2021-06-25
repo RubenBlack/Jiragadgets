a46cebcc715c4850be5f10d3810caf6a1ab81861
RubenBlack <86473347+RubenBlack@users.noreply.github.com>
2021-06-25 16:04:19 +0200

    Add files via upload

---
+++ GadgetCode.groovy
@@ -0,0 +1,12 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.worklog.WorklogManager
+import com.atlassian.jira.bc.issue.worklog.TimeTrackingConfiguration
+import com.atlassian.core.util.DateUtils
+import com.atlassian.jira.issue.*
+import com.atlassian.jira.issue.link.*
+import com.atlassian.jira.issue.resolution.Resolution
+import groovy.xml.*
+
+ def issuekey1 = "Insertando codigo"
+ print "$issuekey1"
+    
